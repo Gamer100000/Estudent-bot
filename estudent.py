9b47f00ac46b6bceb75582218c553cab22918f35
Yonathan Assefa <yab95assefa@gmail.com>
2022-02-11 21:43:13 +0300

    The bot login, view result is added

--- estudent.py
+++ estudent.py
@@ -1,50 +1,111 @@
-from telegram import Update
-
-from telegram.ext import Updater
-from telegram.ext import CallbackContext
-from telegram.ext import CommandHandler, MessageHandler
-from telegram import *
-
 import logging
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+import telegram
 
-updater = Updater(token='', use_context=True)
-dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-
-
-
-def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-# def login():
-# 	context.bot.send_message(chat_id=update.effective_chat.id, text="Hello")
-
-def login(user_input):
-    answer = "You have wrote me " + user_input
-    return answer
-
-def reply(update, context):
-    user_input = update.message.text
-    update.message.reply_text(do_something(user_input))
+import os
+from dotenv import load_dotenv	
 
 
-def main():
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text, reply))
-    updater.start_polling()
-    updater.idle()
+from estudent_bot import estudent_login, list_subjects, view_result
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, Filters, MessageHandler
+
+load_dotenv()
 
 
-another_handler = CommandHandler('menu', login)
-dispatcher.add_handler(start_handler)
+TELEGRAM_HTTP_API_TOKEN = os.getenv('token')
+FIRST, SECOND, THIRD, FOURTH, FIVTH = range(5)
 
-# updater.start_polling()	
+def start(update, bot):
+    keyboard = [
+        [InlineKeyboardButton(u"Login", callback_data=str(FIRST))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(
+        u"Estudent Bot",
+        reply_markup=reply_markup
+    )
+    return FIRST
 
-main()
+def first(update, bot):
+	print("I am first")
+	query = update.callback_query
+	query.message.reply_text(u"Enter Your ID")
+	return SECOND
+
+def second(update, bot):
+	global user_id
+	print("I am Second")
+	query = update.message
+	user_id = update.message.text
+	print(user_id)
+	query.reply_text(u"Enter Your Password")
+	return THIRD
+
+def third(update, bot):
+	global user_password, user_id
+	print("I am third")
+	query = update.message
+	user_password = query.text
+	print(user_password)
+
+	keyboard = [
+		[InlineKeyboardButton(u"View Result", callback_data=str(FOURTH))]]
+	reply_markup = InlineKeyboardMarkup(keyboard)
+	
+	print(estudent_login(user_id, user_password))
+
+	update.message.reply_text(
+	    u"Logged in Successfully",
+	    reply_markup=reply_markup
+	)
+
+	return FOURTH
 
 
+def fourth(update, bot):
+	print("I am fourth")
+	query = update.callback_query
+	global courses
+	courses = list_subjects()
+	keyboard = []
+	for course in courses:
+		keyboard.append([InlineKeyboardButton(course, callback_data=str(course))])
+	reply_markup = InlineKeyboardMarkup(keyboard)
+	query.message.reply_text(
+	    u"Please Select a Course",
+	    reply_markup=reply_markup)
+	query.message.reply_text(u"Success")
+	return FIVTH
+
+def fivth(update, bot):
+	print("I am fivth")
+	query = update.callback_query
+	query.answer()
+	print(query.data)
+	query.message.reply_text(view_result(query.data), parse_mode=telegram.ParseMode.HTML)	
+	
+    
+
+updater = Updater(TELEGRAM_HTTP_API_TOKEN, use_context=True)
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+    states={
+        FIRST: [CallbackQueryHandler(first)],
+        SECOND: [MessageHandler(Filters.text, second)],
+        THIRD: [MessageHandler(Filters.text, third)],
+        FOURTH: [CallbackQueryHandler(fourth)],
+        FIVTH: [CallbackQueryHandler(fivth)],
+        
+    },
+    fallbacks=[CommandHandler('start', start)]
+)
+
+updater.dispatcher.add_handler(conv_handler)
+
+updater.start_polling()
+
+updater.idle()
--- estudent_bot.py
+++ estudent_bot.py
@@ -7,7 +7,8 @@ from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import TimeoutException
 import time
 from tabulate import tabulate
-import getpass
+
+
 
 options = Options()
 options.headless = True
@@ -18,12 +19,12 @@ driver.get("http://10.240.1.89")
 user_id = driver.find_element(By.ID,"user_name")
 password = driver.find_element(By.ID, "password")
 sign = driver.find_element(By.TAG_NAME, "button")
+delay = 2 # seconds
 
-logged_in = False
+def estudent_login(u_id,u_password):
 
-while not logged_in:
-	u_id = input("Id: ")
-	u_password = getpass.getpass("Password: ")
+	# --------------
+	
 	user_id.send_keys(u_id)
 	password.send_keys(u_password)
 	sign.click()
@@ -31,46 +32,51 @@ while not logged_in:
 		error = driver.find_element (By.XPATH, "//div[contains( text(), 'Invalid login credentials. Please try again.')]")
 		user_id.clear()
 		password.clear()
-		print("Incorrect id or password")
+		return False
+		
 	except:
-		logged_in = True
-		print("logged in successfully")
+		return True
 
-# After loged in successfully
-delay = 2 # seconds
-time.sleep(delay)
-ac_his = driver.find_element(By.XPATH, "//a[@href='/academic_history']")
-ac_his.click()
+	# After loged in successfully
+	
+	time.sleep(delay)
 
-#Academic History
-time.sleep(delay)
+
+def list_subjects():
+	global courses
+	ac_his = driver.find_element(By.XPATH, "//a[@href='/academic_history']")
+	ac_his.click()
+	#Academic History
+	time.sleep(delay)
+	
+
+	assessment_result = driver.find_element(By.XPATH, "//a[@href='#assessment_result']")
+	assessment_result.click()
+
+	time.sleep(delay)
+
+	#Scroll course
+	scroll = driver.find_element(By.ID, 'course_enrollment_id')
+	options = [x.text for x in scroll.find_elements(By.TAG_NAME,"option")]
+	options_a = [x.get_attribute("value") for x in scroll.find_elements(By.TAG_NAME,"option")]
+
+	courses = {}
+
+	for i in range(len(options)):
+		courses[options[i]] = options_a[i]
+
+	# for i,c in enumerate(options):
+	# 	print(i,c)
+	return courses
+
+
+	
 
 def view_result(value):
-	driver.find_element(By.XPATH,f"//option[@value='{value}']").click() 
-
-assessment_result = driver.find_element(By.XPATH, "//a[@href='#assessment_result']")
-assessment_result.click()
-
-time.sleep(delay)
-
-#Scroll course
-scroll = driver.find_element(By.ID, 'course_enrollment_id')
-options = [x.text for x in scroll.find_elements(By.TAG_NAME,"option")]
-options_a = [x.get_attribute("value") for x in scroll.find_elements(By.TAG_NAME,"option")]
-
-courses = {}
-for i in range(len(options)):
-	courses[options[i]] = options_a[i]
-
-for i,c in enumerate(options):
-	print(i,c)
-
-print(courses)
-while True:
+	global courses
 	try:
-		course = int(input())
-		view_result(courses[options[course]])
-
+		value = courses[value]
+		driver.find_element(By.XPATH,f"//option[@value='{value}']").click() 
 
 		result_table = driver.find_element(By.XPATH, '/html/body/div/div/div/section/div/div/div[2]/div/div[4]/div/div/div/div/div/div[2]/table')
 		t_head = driver.find_element(By.CLASS_NAME, 'thead-light')
@@ -90,6 +96,7 @@ while True:
 				d.append(i.text)
 
 		print(tabulate(data,headers=thead))
+		return tabulate(data,headers=thead)
 	except:
 		print("Exiting...")
-		break
+		return False
